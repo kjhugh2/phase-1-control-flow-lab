40b2ed559c4e30f8173b963bb7d7b5a52cf07049
Kendall Hughes <kjhugh2@gmail.com>
2022-03-26 23:13:44 -0500

    commit tree a35a3eea63397e6f37ddf38d2ff7d33a5487e122

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+  let result
+  if (someNumber <= 400 ) {
+   return 'This one is on me!';
+  }
+  else if (someNumber >= 2000 && someNumber < 2500 ){
+    return 'I will gladly take your thirty bucks.';
+  }
+  else {
+ return 'No can do.'
 }
+}
+{
+  let city = prompt('Ok, sounds good.');
+  city === 'NYC'
+}
+
 
 function ternaryCheckCity(){
   // Write your code here!
